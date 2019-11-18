bcdf6a7fdf856a54d1a95348f506d2582754b823
unknown <rakhtim.chatterjee@stonybrook.edu>
2019-11-17 22:42:35 -0500

    Basic changes to handle ip based routing and round-robin scheduling of requests

--- client.py
+++ client.py
@@ -2,16 +2,22 @@
 import socket
 
 # Create a socket object
-s = socket.socket()
+def client_process():
+    s = socket.socket()
 
-# Define the port on which you want to connect
-port = 6789
+    # Define the port on which you want to connect
+    lb_port = 6789
 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
+    # connect to the server on local computer
+    s.connect(('127.0.0.1', lb_port))
 
-# receive data from the server
-print s.recv(1024)
-print s.recv(1024)
-# close the connection
-s.close()
+    # receive data from the server
+    print (s.recv(1024))
+    print (s.recv(1024))
+    # close the connection
+    s.close()
+
+client_process()
+client_process()
+client_process()
+client_process()
--- server1.py
+++ server1.py
@@ -2,13 +2,13 @@
 import socket
 
 # next create a socket object
-def loadbal_process():
+def server_process_1():
     s = socket.socket()
     print ("Socket successfully created")
 
     # reserve a port on your computer in our
     # case it is 12345 but it can be anything
-    port = 6789
+    port = 12345
 
     # Next bind to the port
     # we have not typed any ip in the ip field
@@ -22,42 +22,17 @@ def loadbal_process():
     s.listen(5)
     print ("socket is listening")
 
-    sever_req_count = 0
     # a forever loop until we interrupt it or
     # an error occurs
     while True:
         # Establish connection with client.
-        print ('new')
         c, addr = s.accept()
         print ('Got connection from', addr)
-
         # send a thank you message to the client.
-        c.send('Thank you for connecting to load balancer')
-
-        sever_req_count = sever_req_count + 1
-
-        s2 = socket.socket()
-
-        # if(sever_req_count%2 == 1):
-        #     port2 = 12345
-        # else:
-        #     port2 = 45678
-
-        if (addr[1]^11000 >=50000 and addr[1]^11000 <=60000):
-            port2 = 12345
-        else:
-            port2 = 45678
-
-
-
-        # connect to the server on local computer
-        s2.connect(('127.0.0.1', port2))
-        # receive data from the server
-        c.send(s2.recv(1024))
-        # close the connection
-        # s.close()
+        c.send('Thank you for connecting to server 1')
 
         # Close the connection with the client
         c.close()
 
-loadbal_process()
+
+server_process_1()
