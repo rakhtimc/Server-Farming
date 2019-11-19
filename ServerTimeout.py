02825d8b603e61c1af72e0fd1b0d92920fef5adf
Rohit Rao Rayachoti <rohitrrao.96@gmail.com>
2019-11-19 02:27:53 -0500

    Adding multithreading functionality in servertimeout

--- ServerTimeout.py
+++ ServerTimeout.py
@@ -1,55 +1,105 @@
 # first of all import the socket library
 import socket
 import time
+import threading
+from Queue import Queue
 
 # next create a socket object
 from server import ProcessRequest
 
-
-
-
-s = socket.socket()
-print "Socket successfully created"
-
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 12345
-
-request_count = 0
+numthreads = 2
+numjobs = 10
+thread_queue = Queue()
+s = socket
 conns = []
 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-s.bind(('', port))
-print "socket binded to %s" % (port)
-st = time.time()
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
-    s.listen(10000)
-    print "socket is listening"
-    # Establish connection with client.
-    c, addr = s.accept()
+def create_socket():
+    global s
+    s = s.socket()
+    print "Socket successfully created"
 
-    conns.append(c)
+    # reserve a port on your computer in our
+    # case it is 12345 but it can be anything
+    port = 12345
 
-    if len(conns) > 2:
-        print 'Show Some Mercy on Server!!'
-        c.send('Show Some Mercy on Server!!')
-        c.close()
-    else:
+    request_count = 0
+    conns = []
+
+    # Next bind to the port
+    # we have not typed any ip in the ip field
+    # instead we have inputted an empty string
+    # this makes the server listen to requests
+    # coming from other computers on the network
+    s.bind(('', port))
+    print "socket binded to %s" % (port)
+    return s
+
+def listen_conn(sock):
+    st = time.time()
+    # a forever loop until we interrupt it or
+    # an error occurs
+    while True:
+        sock.listen(50)
+        print "socket is listening"
+        # Establish connection with client.
+        c, addr = sock.accept()
+
         print 'Got connection from', addr
 
-        # send a thank you message to the client.
         resp = ProcessRequest('')
         c.send(resp.handleRequest())
 
         if len(conns) > 0 and (time.time() - st) > 2:
             conns.pop()
             st = time.time()
-        # Close the connection with the client
-        # time.sleep(request_count)
-        # c.close()
+
+        # conns.append(c)
+        # if len(conns) > 2:
+        #     print 'Show Some Mercy on Server!!'
+        #     c.send('Show Some Mercy on Server!!')
+        #     c.close()
+        # else:
+        #     print 'Got connection from', addr
+        #
+        #     resp = ProcessRequest('')
+        #     c.send(resp.handleRequest())
+        #
+        #     if len(conns) > 0 and (time.time() - st) > 2:
+        #         conns.pop()
+        #         st = time.time()
+
+def thread_work():
+    for _ in range(numthreads):
+        global s
+        while True:
+            x = thread_queue.get()
+            listen_conn(s)
+            # if x == 1:
+            #     s = create_socket()
+            #     listen_conn(s)
+            # elif x == 2:
+            #     listen_conn(s)
+
+            thread_queue.task_done()
+
+def create_workers():
+    t = threading.Thread(target=thread_work)
+    t.daemon = True
+    t.start()
+
+def create_jobs():
+    i = 0
+    while i < numthreads:
+        thread_queue.put(i)
+        i = i + 1
+    thread_queue.join()
+
+def main():
+    global s
+    s = create_socket()
+    listen_conn(s)
+    create_workers()
+    create_jobs()
+
+if __name__ == '__main__':
+    main()
